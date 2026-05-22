e49825685eb77515b18b7d6b99f26fea2d2aaeed
IPeKB Buleleng <ipekbbuleleng@gmail.com>
2026-05-22 14:19:23 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // E-PKB Kabupaten Buleleng - Service Worker ringan
-// Version: 0.2.1-LAPOR-KEGIATAN-PKB-V1-R1
+// Version: 0.2.2-LAPOR-KEGIATAN-PKB-V1-R2
 self.addEventListener('install', function(event) {
   self.skipWaiting();
 });
